da1faaedd2bb8177a3d11920f1f75df82573a41b
LxyFreely <103030034+LxyFreely@users.noreply.github.com>
2026-06-28 20:52:29 +0800

    read

--- screen.lua
+++ screen.lua
@@ -1,14 +1,15 @@
 scr=peripheral.find("Create_DisplayLink")
 --scr=peripheral.find("create_source")
+print(scr.getSize())
 tar=peripheral.find("create_target")
 print(scr.isColor())
 tar.resize(100,128)
 
 function getSteam()
-    steam1=string.sub(tar.getLine(1),21)
-    steam2=string.sub(tar.getLine(5),21)
-    steam3=string.sub(tar.getLine(9),21)
-    steam4=string.sub(tar.getLine(13),21)
+    steam1=tar.getLine(1):sub(21,21)
+    steam2=tar.getLine(5):sub(21,21)
+    steam3=tar.getLine(9):sub(21,21)
+    steam4=tar.getLine(13):sub(21,21)
 end
 
 function colorb(c)
@@ -16,14 +17,54 @@ function colorb(c)
     scr.write(c)
 end
 
-while true do
-    scr.clear()
+function displayFile(filePath,scr)
+    startLine=1
+    local file=fs.open(filePath,"r")
+    if not file then
+        print("open failed"..filePath)
+        return false
+    end
     
-    getSteam()
-    scr.setCursorPos(1,1)
-    colorb("u")
-    scr.writeBytes({233,148,133,231,130,137,231,173,137,231,186,167})
-    scr.setCursorPos(1,2)
-    scr.update()
-    sleep(1)
+    local lineNum=startLine
+    while true do
+        local line=file.readLine()
+        if not line then break end
+        
+        local bytes={}
+        for numStr in string.gmatch(line, "[^,]+") do
+            numStr=numStr:match("^%s*(.-)%s*$")
+            local num=tonumber(numStr)
+            if num then
+                table.insert(bytes,num)
+            else
+                print("warning,found not number at ..lineNum")
+            end
+        end
+        
+        scr.setCursorPos(1,lineNum)
+        
+        if #bytes>0 then
+            scr.writeBytes(bytes)
+        end
+        
+        scr.update()
+        
+        lineNum=lineNum+1
+    end
+    
+    file.close()
+    return true
 end
+
+
+
+
+scr.clear()
+scr.update()
+
+displayFile("data.txt",scr)
+
+--while true do
+
+
+
